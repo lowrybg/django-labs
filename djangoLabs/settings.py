1170172391ba920c8056d731825031be0c90b556
lowry <lowrybg@users.noreply.github.com>
2026-01-30 21:57:11 +0200

    failed migrations, found and add missing project apps in settings.py

--- djangoLabs/settings.py
+++ djangoLabs/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+] + PROJECT_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
