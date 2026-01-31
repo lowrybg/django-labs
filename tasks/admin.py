b149347453049593394eefe35f14c77f337f288f
lowry <lowrybg@users.noreply.github.com>
2026-01-31 20:45:11 +0200

    create superuser, add some decoration

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from tasks.models import Task
+from django.conf import settings
+import os
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['title','is_done']
+    search_fields = ['title']
+    list_filter = ['is_done']
+#    save_as = True
+
+
+
