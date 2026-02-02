efb51232af2dc8e98b335e40fe6e2bdbaa1faa82
lowry <lowrybg@users.noreply.github.com>
2026-02-02 22:29:03 +0200

    Update urls.py

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,11 @@
+import uuid
+
 from django.urls import path
 
-from tasks.views import index
+from tasks.views import index, id_view, uuid_view
 
 urlpatterns = [
     path('', index),
+    path('<num>/',id_view),
+    path('<uuid>/',uuid_view),
 ]
